5e527caa2ae2d0d983e18cc27e5e97ebc19cd497
syedramsha <ramsha.syed464@gmail.com>
2024-07-11 14:12:16 +0200

    feat: Add Designs From Styled components to Tailwind only

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Input from "./Input.jsx";
+import Button from "./Button.jsx";
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -22,26 +23,29 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <Input
-          label="Email"
-        type="email"
-        invalid={emailNotValid}
-        onChange={(event) => handleInputChange('email', event.target.value)}
-      />
-      <Input
-          label="Password"
-        type="password"
-        invalid={passwordNotValid}
-        onChange={(event) =>
-          handleInputChange('password', event.target.value)
-        }
-      />
-      <div className="actions">
-        <button type="button" className="text-button">
+    <div id="auth-inputs " className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800 mx-auto ">
+    <div className="felx flex-col gap-2 mb-6">
+        <Input
+            label="Email"
+            type="email"
+            invalid={emailNotValid}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+        />
+        <Input
+            label="Password"
+            type="password"
+            invalid={passwordNotValid}
+            onChange={(event) =>
+                handleInputChange('password', event.target.value)
+            }
+        />
+    </div>
+
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
